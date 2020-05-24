0fa7e2982140f5e496290907f6021bb05097f370
Roberto Carnaúba Dias <betocarnauba@hotmail.com>
2020-05-24 17:12:03 -0300

    services
    Ultilizando XML e Json.
    Precisa das libs.

--- src/br/alura/gerenciador/servelet/EmpresasService.java
+++ src/br/alura/gerenciador/servelet/EmpresasService.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
 
 import br.alura.gerenciador.modelo.Banco;
 import br.alura.gerenciador.modelo.Empresa;
@@ -22,13 +22,19 @@ public class EmpresasService extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		List<Empresa> empresas = new Banco().getEmpresas();
+//		//retorno em JSon
+//		Gson gson = new Gson();
+//		String json = gson.toJson(empresas);
+//		
+//		response.setContentType("application/json");
+//		response.getWriter().print(json);
+//		
+		XStream xStream = new XStream();
+		xStream.alias("empresas", Empresa.class);
+		String xml = xStream.toXML(empresas);
 		
-		Gson gson = new Gson();
-		String json = gson.toJson(empresas);
-		
-		response.setContentType("application/json");
-		response.getWriter().print(json);
-		
+		response.setContentType("application/xml");
+		response.getWriter().print(xml);
 	}
 
 }
